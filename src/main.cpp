383395cd436246510996cf97ec33d89cbc7ba269
s.eriksson89@gmail.com <s.eriksson89@gmail.com>
2019-12-07 14:34:00 +0100

    Add main return codes

--- src/main.cpp
+++ src/main.cpp
@@ -12,36 +12,17 @@ int main(int argc, char* argv[])
     auto file_buffer = tent::file_reader::to_buffer(argv[1]);
     lt::error_code error;
     
-    const lt::torrent_info torrent_info{file_buffer.data(), static_cast<int>(file_buffer.size()), error};
-
-    // std::printf("\n\n----- torrent file info -----\n\n"
-    // "nodes:\n");
-    // for (auto const& i : torrent_info.nodes())
-    //     std::printf("%s: %d\n", i.first.c_str(), i.second);
-
-    // puts("trackers:\n");
-    // for (auto const& i : torrent_info.trackers())
-    //     std::printf("%2d: %s\n", i.tier, i.url.c_str());
-
-    // std::stringstream ih;
-    // ih << torrent_info.info_hash();
-    // std::printf("number of pieces: %d\n"
-    //     "piece length: %d\n"
-    //     "info hash: %s\n"
-    //     "comment: %s\n"
-    //     "created by: %s\n"
-    //     "name: %s\n"
-    //     "number of files: %d\n"
-    //     "files:\n"
-    //     , torrent_info.num_pieces()
-    //     , torrent_info.piece_length()
-    //     , ih.str().c_str()
-    //     , torrent_info.comment().c_str()
-    //     , torrent_info.creator().c_str()
-    //     , torrent_info.name().c_str()
-    //     , torrent_info.num_files());
+    const lt::torrent_info torrent_info{file_buffer.data(), 
+        static_cast<int>(file_buffer.size()), error};
+    
+    if(error.value() != 0)
+    {
+        std::cerr << "Failed to read torrent: " << error.message() << std::endl;
+        return EXIT_FAILURE;
+    }
 
     tent::tracker_client tracker_client{torrent_info};
-    tracker_client.announce(12345);
-  
+    tracker_client.announce(12345);  
+
+    return EXIT_SUCCESS;
 }
