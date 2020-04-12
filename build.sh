6ceaae3f43a11028aa44e47cf9c505a11ba14cbe
s.eriksson89@gmail.com <s.eriksson89@gmail.com>
2020-04-12 14:20:39 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ROOT=`pwd`
+LIBTORRENT_BUILD_DIR=libtorrent/build
+BUILD_DIR=build
+
+# TODO: parse input
+# rm -rf libtorrent/build 
+# rm -rf build
+
+if [ ! -d "$LIBTORRET_BUILD_DIR" ]; then 
+    mkdir "$LIBTORRET_BUILD_DIR"
+fi
+
+cd "$LIBTORRET_BUILD_DIR"
+cmake ..
+make 
+cd $ROOT 
+
+if [ ! -d "$BUILD_DIR" ]; then 
+    mkdir "$BUILD_DIR"
+fi
+
+cd "$BUILD_DIR"
+cmake ..
+make
